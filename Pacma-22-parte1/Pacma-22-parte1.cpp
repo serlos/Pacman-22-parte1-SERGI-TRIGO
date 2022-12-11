aea96db86f1128367dd65d0aee6ce590d5023db6
PC-serlos <a@a.com>
2022-12-11 18:37:17 +0100

    Version final.

--- Pacma-22-parte1/Pacma-22-parte1.cpp
+++ Pacma-22-parte1/Pacma-22-parte1.cpp
@@ -1,34 +1,105 @@
 // Pacma-22-parte1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+/// <summary>Creo macros para no tener numeros sueltos, defino sets con el enum y con al map relleno el array
+// y guarda lo que tengo en cada una de las casillas. Con el bool se si se esta ejecutando o no la apliación</summary>
+
 #include <iostream>
-#define MAP_VERTICAL 29   // creamos macros para que no nos queden números sueltos y asegurarnos no equivocarnos
+#include <stdlib.h>
+#define MAP_VERTICAL 29   
 #define MAP_HORIZONTAL 120
-enum TILES {EMPTY = ' ', WALL ='#', POINT='.' }; //enum define un set y nos da un entero
-TILES map[MAP_VERTICAL][MAP_HORIZONTAL]; //lo ponemos aqui para poder acceder de donde queramos /reservo memoria, rellenamos nuestro array / el map guarda que es lo que tengo en cada una de las casillas
-bool run = true; //para saber si estamos ejecutando la apliación o no
+#define PERSONAJE 'O' 
+enum TILES {EMPTY = ' ', WALL ='#', POINT='.' };
+TILES map[MAP_VERTICAL][MAP_HORIZONTAL]; 
+bool run = true; 
 enum INPUT {QUIT, UP, DOWN , LEFT, RIGHT, UNKNOWN};
 INPUT currentInput = INPUT::UNKNOWN;
+int personaje_x;
+int personaje_y;
+int puntuacion_actual;
+int puntuacion_total;
+
+
+/// <summary>con Setup se ve el alto y ancho de la consola (relleno mi mapa) y declaramos donde queremos los #
+// es decir los limites y añado color.</summary>
 
 void Setup()
 { 
-	for (size_t i = 0; i < 29; i++)   //vemos el alto de la pantalla de la consola
+	system("Color 03");
+	personaje_x = MAP_HORIZONTAL / 2;
+	personaje_y = MAP_VERTICAL / 2;
+	for (size_t i = 0; i < 29; i++)   
 	{
-		for (size_t j = 0; j < 120; j++)   //vemos el ancho de la pantalla de la consola
+		for (size_t j = 0; j < 120; j++)   
 		{
-			if (i == 0 || j == 0 || i == MAP_VERTICAL - 1 || j == MAP_HORIZONTAL - 1) //declaramos donde queremos el #
+			if (i == 0 || j == 0 || i == MAP_VERTICAL - 1 || j == MAP_HORIZONTAL - 1) 
 			{
-				map[i][j] = TILES::WALL;    //inicializamos la memoria con un valor
+				map[i][j] = TILES::WALL;    
 			}
 			else
 			{
-				map[i][j] = TILES::EMPTY; //prueba
+				map[i][j] = TILES::EMPTY; 
 
 			}
 		}
 
 	}
+	/// <summary>Añado puntos y huecos por el mapa</summary>
+
+	
+	map[10][10] = TILES::POINT; 
+	map[10][11] = TILES::POINT; 
+	map[10][12] = TILES::POINT; 
+	map[10][13] = TILES::POINT; 
+	map[10][14] = TILES::POINT; 
+	map[10][15] = TILES::POINT; 
+	map[10][16] = TILES::POINT; 
+	map[9][10] = TILES::POINT; 
+	map[9][11] = TILES::POINT; 
+	map[9][12] = TILES::POINT; 
+	map[9][13] = TILES::POINT; 
+	map[9][14] = TILES::POINT; 
+	map[9][15] = TILES::POINT; 
+	map[9][16] = TILES::POINT; 
+	map[9][17] = TILES::POINT; 
+	
+	
+	map[11][0] = TILES::EMPTY; 
+	map[12][0] = TILES::EMPTY;
+	map[13][0] = TILES::EMPTY;
+	map[14][0] = TILES::EMPTY;
+
+	map[11][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[12][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[13][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+	map[14][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+
+	map[0][59] = TILES::EMPTY;
+	map[0][60] = TILES::EMPTY;
+	map[0][61] = TILES::EMPTY;
+	map[0][62] = TILES::EMPTY;
+
+	map[MAP_VERTICAL - 1][59] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][60] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][61] = TILES::EMPTY;
+	map[MAP_VERTICAL - 1][62] = TILES::EMPTY;
+
+	/// <summary>compruebo los puntos que tengo en mi mapa y me dice mi puntuacion total</summary>
+
+	for (size_t i = 0; i < 29; i++)   
+	{
+		for (size_t j = 0; j < 120; j++)   
+		{
+			if (map[i][j] == TILES::POINT)
+			{
+				puntuacion_total++;
+			}
+		}
+
+	}
+
 }
+/// <summary>Input indico hacia donde se movera mi personaje con los inputs que le estoy indicando</summary>
 
 void Input()
 {
@@ -62,43 +133,89 @@ void Input()
 	}
 }
 
+/// <summary>Logic Indico que cuando introduzca la Q el juego terminará y además compruebo la colision del personaje
+// y hago que cuando recoja un punto ese lo rellene de "vacio", tambien hago que cuando superemos un limite donde
+// no haya una pared aparezca por el limite contrario</summary>
 void Logic()
 {
+	int personaje_y_new = personaje_y;
+	int personaje_x_new = personaje_x;
 	switch (currentInput)
 	{
 	case QUIT:
 		run = false;
 		break;
 	case UP:
+		personaje_y_new--;
 		break;
 	case DOWN:
+		personaje_y_new++;
 		break;
 	case LEFT:
+		personaje_x_new--;
 		break;
 	case RIGHT:
+		personaje_x_new++;
 		break;
 	
 	}
-}
-
-void Draw()
-{
-	system("CLS"); //ponemos una funciond e WINDOWS clean screen para q se vea aceptable
-	for (size_t i = 0; i < 29; i++)   //imprimimos esa memoria para imprimirla por pantalla
+	if (personaje_x_new < 0) 
 	{
-		for (size_t j = 0; j < 120; j++)
-		{
-			std::cout << (char)map[i][j]; //ponemos el char para decir en que queremos transformar el enum y no salga el numero entero asignado
-		}
-		std::cout << std::endl;
+		personaje_x_new = MAP_HORIZONTAL - 1;
+	}
+	personaje_x_new = personaje_x_new % MAP_HORIZONTAL;
+	
+	if (personaje_y_new < 0) 
+	{
+		personaje_y_new = MAP_VERTICAL - 1;
+	}
+	personaje_y_new = personaje_y_new % MAP_VERTICAL;
+
+	switch (map[personaje_y_new][personaje_x_new])
+	{
+	case TILES::WALL:
+		break;
+	case TILES::POINT:
+		puntuacion_actual++;
+		map[personaje_y_new][personaje_x_new] = TILES::EMPTY; 
+	default:
+		personaje_y = personaje_y_new;
+		personaje_x = personaje_x_new;
+		break;
 	}
 }
 
+/// <summary>Draw añado una funcion de windows CLS(borrar pantalla) para que se pueda ver de mejor todo
+// también indicamos char ya que nuestro mapa es un array de 2D y asi indicamos en que queremos
+// transformar nuestro  enum y no salga el numero entero asignado a ese simbolo/carácter
+// finalmente añadimos en la ultima linea la puntuacion actual y total</summary>
+void Draw()
+{
+	system("CLS"); 
+	for (size_t i = 0; i < 29; i++)   
+	{
+		for (size_t j = 0; j < 120; j++)
+		{
+			if (i == personaje_y && j == personaje_x) 
+			{
+				std::cout << PERSONAJE;
+			}
+			else
+			{
+				std::cout << (char)map[i][j]; 
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << puntuacion_actual << '/' << puntuacion_total;
+}
+
+/// <summary>main le indicamos lo que hara mientras se ejecute</summary>
 int main() 
 {
 	Setup();
 	Draw();
-	while (run) //le indicamos lo que hace mientras se este ejecutando
+	while (run) 
 	{
 		Input();
 		Logic();
